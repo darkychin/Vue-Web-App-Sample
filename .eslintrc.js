2eae1848174188c2b4ed48d89ebd0a4b237a3b85
Darky <darkychin95@gmail.com>
2021-11-20 20:50:51 +0800

    Update .eslintrc.js
    + Update max-len rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,14 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Add max len configuration
+    'max-len': [
+      'error', {
+        code: 180,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+      },
+    ],
   },
 };
