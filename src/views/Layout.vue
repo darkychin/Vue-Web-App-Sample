9659c1900cf446874e246e3cfc3ac51310513d53
Darky <darkychin95@gmail.com>
2021-11-20 18:36:26 +0800

    Create Layout.vue

---
+++ src/views/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <the-navbar /> -->
+    <div>
+      <router-view />
+    </div>
+    <!-- <the-footer /> -->
+  </div>
+</template>
+
+<script>
+// Components
+// import TheNavbar from
+// import TheFooter from
+export default {
+  name: 'Layout',
+};
+</script>
