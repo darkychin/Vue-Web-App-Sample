2dcd20872e71723a155ea9f1dcb44a97bb7120b6
Darky <darkychin95@gmail.com>
2022-05-21 23:37:14 +0800

    Update router/index.js
    + Add gallery routes
    + Add comments

--- src/router/index.js
+++ src/router/index.js
@@ -5,30 +5,33 @@ import VueRouter from 'vue-router';
 import Layout from '@/views/Layout.vue';
 import LayoutAdmin from '@/views/LayoutAdmin.vue';
 import Landing from '@/views/Landing.vue';
-import Home from '../views/Home.vue';
 
 // Routes
+import GalleryRoutes from './gallery';
 
 // Lazy Loading
 // const News = () => import(/* webpackChunkName: "news" */ '@/views/News.vue');
+const ServiceExample = () => import(/* webpackChunkName: "service" */ '@/views/ServiceExample.vue');
 
 Vue.use(VueRouter);
 
+// Important: Route order matters!
 const routes = [
-  // Normal user layout
+  /**
+   * Normal User Layout
+   *
+   * This act as a parent component that wraps all of its children, and they will share the same layout structure
+   * Reference: https://router.vuejs.org/guide/essentials/nested-routes.html
+   */
   {
     path: '/',
     component: Layout,
     children: [
       {
+        // The default to go to when '/' is routed
         path: '',
         name: 'Landing',
         component: Landing,
-      },
-      {
-        path: '/home',
-        name: 'Home',
-        component: Home,
       },
       {
         path: '/about',
@@ -38,6 +41,17 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
       },
+      {
+        path: '/service-example',
+        name: 'ServiceExample',
+        component: ServiceExample,
+      },
+      /**
+       * Using separate routes file can keep the route cleaner
+       * Reference:
+       * @see {@link https://stackoverflow.com/a/59139467/7939633}
+       */
+      ...GalleryRoutes,
     ],
   },
   // Admin layout
